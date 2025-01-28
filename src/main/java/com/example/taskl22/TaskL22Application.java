ad71ef2237d825779587b5be3c37535c70b1fb5b
farid-hacibabazade <faridhaci0@gmail.com>
2025-01-28 15:53:27 +0400

    initial commit

---
+++ src/main/java/com/example/taskl22/TaskL22Application.java
@@ -0,0 +1,13 @@
+package com.example.taskl22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskL22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskL22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskl22/TaskL22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskl22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskL22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
